ec5574cd302f94ad8ca1c330db6f08e21cf3e25f
par1tet <mankova486@gmail.com>
2025-01-26 23:04:16 +0300

    feat: gradient animation of triangle
    P.S Lev sucks

--- assets/fragment.glsl
+++ assets/fragment.glsl
@@ -4,9 +4,55 @@ in vec4 vertexColor;
 
 out vec4 color;
 
-uniform vec3 greenColor;
+uniform float phaseValue;
+uniform int numberPhase;
 
 void main()
 {
-    color = vec4(vertexColor.xyz,  1.0f);
+    vec3 animatedColor = vec3(vertexColor.xyz);
+
+    float tempZero = animatedColor[0];
+
+    if(numberPhase == 3){
+        tempZero = animatedColor[0];
+        animatedColor[0] = animatedColor[1];
+        animatedColor[1] = animatedColor[2];
+        animatedColor[2] = tempZero;
+    }
+
+    if(numberPhase == 2){
+        tempZero = animatedColor[0];
+        animatedColor[0] = animatedColor[1];
+        animatedColor[1] = animatedColor[2];
+        animatedColor[2] = tempZero;
+
+        tempZero = animatedColor[0];
+        animatedColor[0] = animatedColor[1];
+        animatedColor[1] = animatedColor[2];
+        animatedColor[2] = tempZero;
+    }
+
+    if(numberPhase == 3){
+        tempZero = animatedColor[0];
+        animatedColor[0] = animatedColor[1];
+        animatedColor[1] = animatedColor[2];
+        animatedColor[2] = tempZero;
+
+        tempZero = animatedColor[0];
+        animatedColor[0] = animatedColor[1];
+        animatedColor[1] = animatedColor[2];
+        animatedColor[2] = tempZero;
+
+        tempZero = animatedColor[0];
+        animatedColor[0] = animatedColor[1];
+        animatedColor[1] = animatedColor[2];
+        animatedColor[2] = tempZero;
+    }
+    tempZero = animatedColor[0];
+
+    animatedColor[0] += (animatedColor[1] - animatedColor[0]) * phaseValue;
+    animatedColor[1] += (animatedColor[2] - animatedColor[1]) * phaseValue;
+    animatedColor[2] += (tempZero - animatedColor[2]) * phaseValue;
+
+    color = vec4(animatedColor.x, animatedColor.y, animatedColor.z,  1.0f);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -10,44 +10,36 @@
 
 using namespace std;
 
-
-
 int main() {
-    // Иницилизация glfw
     if (!glfwInit()) {
         cerr << "Failed to initialize GLFW" << endl;
         return -1;
     }
-    // Создание окна
+
     GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Window", nullptr, nullptr);
     if (!window) {
         cerr << "Failed to create GLFW window" << endl;
         glfwTerminate();
         return -1;
     }
-    // Добавление окна в контекст что бы оно было видно
     glfwMakeContextCurrent(window);
 
-    // Инициализация glad (загрузка функций OpenGL)
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         cerr << "Failed to initialize GLAD" << endl;
         return -1;
     }
 
-    // Координаты трегуольника
     GLfloat vertices[] = {
-        //x    // y  // z 
          -0.5f,  -0.5f, 0.5f,
          0.5f, -0.5f, 0.5f,
-
          0.0f, 0.5f, -0.5f,
         -0.5f, 0.5f, 0.5f,
     };
 
     GLfloat colors[] = {
-        1.0f, 0.0f, 0.0f,
-        0.0f, 1.0f, 0.0f,
-        0.0f, 0.0f, 1.0f
+        1.0f, 0.5f, 0.2f,
+        0.2f, 1.0f, 0.5f,
+        0.5f, 0.2f, 1.0f
     };
 
     GLuint indices[] = {
@@ -55,10 +47,6 @@ int main() {
         //1, 0, 3
     };
 
-    //VAO (Vertex Array Object): объект массива вершин. Он хранит конфигурацию, которая описывает, 
-    //как интерпретировать данные вершин.
-    //VBO (Vertex Buffer Object): буфер данных вершин, который хранится на GPU для быстрого доступа.
-    
     GLuint VAO, VBO, EBO, CBO;
         
     glGenVertexArrays(1, &VAO);
@@ -66,9 +54,6 @@ int main() {
     glGenBuffers(1, &EBO);
     glGenBuffers(1, &CBO);
 
-    // glBindVertexArray привязывает VAO. Все последующие команды будут записаны в этот VAO.
-    // glBindBuffer привязывает VBO как текущий буфер для массива вершин.
-    // glBufferData загружает данные вершин в GPU.
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -79,17 +64,6 @@ int main() {
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    // glVertexAttribPointer указывает, как читать данные вершин:
-
-    // 0 — номер атрибута (location 0 в шейдере).
-    // 3 — количество значений (X, Y, Z).
-    // GL_FLOAT — тип данных.
-    // GL_FALSE — не нужно нормализовать з#version 330 core
-    // 3 * sizeof(GLfloat) — шаг между вершинами (3 float на вершину).
-    // (GLvoid*)0 — смещение начала данных.
-    // glEnableVertexAttribArray(0)  включает использование атрибута.
-    
     
     glEnableVertexAttribArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -101,53 +75,47 @@ int main() {
 
     glBindVertexArray(0); 
 
-    // glCreateShader создает объект шейдера.
-    // glShaderSource загружает текст шейдера (vertexShaderSource).
-    // glCompileShader компилирует шейдер.     
-
-
-    // Создание программы шейдеров и связывание шейдеров (вершинный + фрагментный)
     GLuint shaderProgram = createShaderProgram("assets/vertex.glsl", "assets/fragment.glsl");
 
-    // Game loop
+    GLint numberPhase = 1; 
+    GLfloat previosPhaseValue = 1.0f;
+
+    // Main loop
     while (!glfwWindowShouldClose(window)) {
-        // Очистка экрана
         glClearColor(0.0f, 0.5f, 0.5f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // Добавление шейдеров в программу
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO); 
 
+        GLfloat timeValue = (glfwGetTime() + 1.0f);
 
-        // Обновляем цвет
-        GLfloat timeValue = glfwGetTime();
+        GLfloat phaseValue = (ceil(timeValue) - timeValue);
+        // cout << timeValue << endl;
 
-        GLfloat firstValue = (sin(timeValue) / 2);
-        GLfloat secondValue = abs((cos(timeValue) / 2) + 0.5f- 1.0f);
-        GLfloat thirdValue = abs((tan(timeValue) / 2) + 0.5f - 1.5f);
+        GLint vertexColorLocationPhaseValue = glGetUniformLocation(shaderProgram, "phaseValue");
+        glUniform1f(vertexColorLocationPhaseValue, phaseValue);
 
-        GLint vertexColorLocationGreen = glGetUniformLocation(shaderProgram, "greenColor");
-        glUniform3f(vertexColorLocationGreen, thirdValue, firstValue, secondValue);
+        if (abs(previosPhaseValue - phaseValue) >= 0.5f){
+            numberPhase += 1;
+        }
+        
+        if (numberPhase == 4){
+            numberPhase = 1;
+        }
 
-        GLint vertexColorLocationBlue = glGetUniformLocation(shaderProgram, "blueColor");
-        glUniform3f(vertexColorLocationBlue, secondValue, thirdValue, firstValue);
-
-        GLint vertexColorLocationRed = glGetUniformLocation(shaderProgram, "redColor");
-        glUniform3f(vertexColorLocationRed, firstValue, secondValue, thirdValue);
-
-
-
-        // Рисование треугольника
+        GLint vertexColorLocationNumberPhase = glGetUniformLocation(shaderProgram, "numberPhase");
+        glUniform1i(vertexColorLocationNumberPhase, numberPhase);
+ 
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
         glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
-        // Swap buffers and poll events (обработка событий)
+
+        previosPhaseValue = phaseValue;
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    // Очистка при заврещении программы
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
 
